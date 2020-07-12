588ab07e05db8618aa0795b2bf250ee899690655
iandj1 <57132463+iandj1@users.noreply.github.com>
2020-07-12 18:56:39 +1000

    Update run.sh

--- duckdns/data/run.sh
+++ duckdns/data/run.sh
@@ -56,7 +56,7 @@ while true; do
     [[ ${IPV4} != *:/* ]] && ipv4=${IPV4} || ipv4=$(curl -s -m 10 "${IPV4}")
     [[ ${IPV6} != *:/* ]] && ipv6=${IPV6} || ipv6=$(curl -s -m 10 "${IPV6}")
 
-    bashio::log.info "https://www.duckdns.org/update?domains=${DOMAINS}&token=${TOKEN}&ip=${ipv4}&ipv6=${ipv6}&verbose=true"
+    #bashio::log.info "https://www.duckdns.org/update?domains=${DOMAINS}&token=${TOKEN}&ip=${ipv4}&ipv6=${ipv6}&verbose=true"
     if answer="$(curl -s "https://www.duckdns.org/update?domains=${DOMAINS}&token=${TOKEN}&ip=${ipv4}&ipv6=${ipv6}&verbose=true")"; then
         bashio::log.info "${answer}"
     else
